c45277d9652fc3fa84c4ba52140b169f8cb6c525
Ran Kirlian <rankirlian@gmail.com>
2022-08-28 17:12:23 +0200

    Add basic middlewares to server

--- src/models/server.js
+++ src/models/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('../routes/users');
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
-
+    this.usersPath = '/users';
     // Database connection
 
     // Middlewares
@@ -16,6 +17,14 @@ class Server {
   }
 
   middlewares() {
+    // For using cors to protect our routes
+    this.app.use(cors());
+
+    // Read and parse of the body to json format
+    this.app.use(express.json())
+
+    // Exposing the public directory
+    this.app.use(express.static('public'));
   };
 
 
